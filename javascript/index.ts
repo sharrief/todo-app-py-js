2f16d20804f14e29e25b04b896240dc084295bc4
Sharrief Shabazz <me@sharrief.com>
2024-10-13 00:59:22 +0000

    Centralize express api routes

--- javascript/index.ts
+++ javascript/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
-import { add, all, remove, update } from '@/todo/routes';
+import apiRoutes from '@/todo/routes';
 import { AppDataSource } from '@/todo_project/settings';
+import path from 'path';
 
 AppDataSource.initialize()
 
@@ -11,12 +12,12 @@ const port = 3000
 
 // express.json() is route handler middleware to parse JSON in request body and serialize response objects to JSON
 app.use(express.json());
-
 app.use(express.static('public'))
-app.get('/all', all)
-app.post('/add', add)
-app.post('/update', update)
-app.post('/delete', remove)
+app.use('/api', apiRoutes)
+app.get('/*', (req, res) => {
+  res.contentType('html')
+  res.sendFile(path.join(__dirname, 'public', 'base.html'));
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- javascript/todo/routes.ts
+++ javascript/todo/routes.ts
@@ -1,5 +1,11 @@
 import { Todo } from '@/todo/models'
-import type { Request, Response } from 'express'
+import { Router, type Request, type Response } from 'express'
+
+export async function getById(req: Request, res: Response) {
+  const id = +req.params.id;
+  const todo = await Todo.findOneBy({ id })
+  res.send(todo)
+}
 
 export async function all(req: Request, res: Response) {
   const todos = await Todo.find()
@@ -28,13 +34,21 @@ export async function update(req: Request<unknown, string, Todo>, res: Response)
   }
 }
 
-export async function remove(req: Request<unknown, string, Todo>, res: Response) {
-  const { body } = req as { body: Todo }
-  const todo = await Todo.findOneBy({ id: body.id })
+export async function remove(req: Request<{ id: string }>, res: Response) {
+  const id = +req.params.id;
+  const todo = await Todo.findOneBy({ id })
   if (todo) {
     await todo.remove()
     res.send(`Deleted todo #${todo.id}: "${todo.title}"`)
   } else {
-    res.send(`Could not find a todo with id ${body.id}:`)
+    res.send(`Could not find a todo with id ${id}:`)
   }
 }
+
+const router = Router()
+router.get('/all', all)
+router.put('/add', add)
+router.patch('/update', update)
+router.delete('/delete/:id', remove)
+router.get('/:id', getById)
+export default router
